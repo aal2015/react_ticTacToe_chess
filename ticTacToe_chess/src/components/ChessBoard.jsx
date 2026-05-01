64e009683c7c880875a391dd78e768b49fe4b74a
aal2015 <abhinavlugun@yahoo.com>
2026-05-01 22:31:09 +0530

    Added chess pieces

--- ticTacToe_chess/src/components/ChessBoard.jsx
+++ ticTacToe_chess/src/components/ChessBoard.jsx
@@ -1,25 +1,65 @@
 import './ChessBoard.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+    faChessPawn,
+    faChessRook,
+    faChessKnight,
+    faChessBishop,
+    faChessQueen,
+    faChessKing
+} from '@fortawesome/free-solid-svg-icons';
+
+const pieceIcons = {
+    wp: faChessPawn,
+    wr: faChessRook,
+    wn: faChessKnight,
+    wb: faChessBishop,
+    wq: faChessQueen,
+    wk: faChessKing,
+
+    bp: faChessPawn,
+    br: faChessRook,
+    bn: faChessKnight,
+    bb: faChessBishop,
+    bq: faChessQueen,
+    bk: faChessKing
+};
+
 const ChessBoard = ({ boardState }) => {
-    console.log(boardState);
     return (
         <div id="chessGameBoard">
             {boardState.map((row, rowIndex) =>
                 row.map((cell, cellIndex) => {
-                    const isWhite = (rowIndex + cellIndex) % 2 === 0;
+                    const isWhiteSquare =
+                        (rowIndex + cellIndex) % 2 === 0;
 
                     return (
                         <div
                             key={`${rowIndex}-${cellIndex}`}
-                            className={`square ${isWhite ? 'whiteSquare' : 'darkBlue'}`}
+                            className={`square ${
+                                isWhiteSquare
+                                    ? 'whiteSquare'
+                                    : 'darkBlue'
+                            }`}
                         >
-                            
+                            {cell && (
+                                <FontAwesomeIcon
+                                    icon={pieceIcons[cell]}
+                                    className={
+                                        cell.startsWith('b')
+                                            ? 'blackPiece'
+                                            : 'whitePiece'
+                                    }
+                                />
+                            )}
                         </div>
                     );
                 })
             )}
-        </ div>
+        </div>
     );
-}
+};
 
 export default ChessBoard;
