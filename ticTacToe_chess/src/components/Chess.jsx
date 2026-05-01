4f8ee20164b66617bb487c5768027140fc75fff8
aal2015 <abhinavlugun@yahoo.com>
2026-05-01 21:32:52 +0530

    Added chess board state

--- ticTacToe_chess/src/components/Chess.jsx
+++ ticTacToe_chess/src/components/Chess.jsx
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+
 const  Chess = () => {
+    const [board, setBoard] = useState([
+        ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br'],
+        ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+        ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr'],
+
+    ]);
     return (<>chess</>);
 }
 
