dcb7342d29a594e0a93a8bc697b14b27f059bb4e
aal2015 <abhinavlugun@yahoo.com>
2026-05-06 23:39:49 +0530

    Added logic for turn move validation

--- ticTacToe_chess/src/components/Chess.jsx
+++ ticTacToe_chess/src/components/Chess.jsx
@@ -16,19 +16,34 @@ const Chess = () => {
     const [turn, setTurn] = useState("white");
     const [selected, setSelected] = useState(null);
 
+    const isMoveValid = (row, col) => {
+        const pieceColor = board[selected[0]][selected[1]][0] === 'w' ? "white" : "black";
+        const piece = board[selected[0]][selected[1]][1]
+        console.log(pieceColor);
+        console.log(piece);
+
+        if (pieceColor !== turn) {
+            return false;
+        }
+
+        return true;
+    }
+
     const pieceSelect = (row, col) => {
         if (selected) {
-            let boardClone = [...board];
-            boardClone[row][col] = boardClone[selected[0]][selected[1]]; 
-            boardClone[selected[0]][selected[1]] = '';
-            setBoard(boardClone);
-            setSelected(null);
+            if (isMoveValid(row, col)) {
+                let boardClone = [...board];
+                boardClone[row][col] = boardClone[selected[0]][selected[1]];
+                boardClone[selected[0]][selected[1]] = '';
+                setBoard(boardClone);
 
-            if (turn === "white") {
-                setTurn("black");
-            } else if (turn === "black") {
-                setTurn("white");
+                if (turn === "white") {
+                    setTurn("black");
+                } else if (turn === "black") {
+                    setTurn("white");
+                }
             }
+            setSelected(null);
             return;
         }
 
