7efe42d968665ce1f5e702a62033cc76e4e1e995
aal2015 <abhinavlugun@yahoo.com>
2026-06-28 23:47:19 +0530

    Added file for ai logic for ches

---
+++ ticTacToe_chess/src/components/ChessAI.js
@@ -0,0 +1,31 @@
+import {
+    isCheckMate
+} from './moveValidCheck';
+
+
+const ChessPiecePoints = {
+    "queen": 9,
+    "rook": 5,
+    "bishop": 3,
+    "knight": 3,
+    "pawn": 3,
+};
+
+class ChessMinMaxAlgo {
+    constructor() {}
+
+    minMax(board, pieceColor, enPassantState, castleState, moveCount) {
+        
+    }
+
+    // isGameEnded(board, pieceColor, enPassantState, moveCount) {
+        
+    // }
+
+    // isCheckMate(
+    //                         boardClone,
+    //                         enemyColor,
+    //                         nextEnPassantState,
+    //                         moveCount
+    //                     );
+}
