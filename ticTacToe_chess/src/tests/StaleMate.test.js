264158d920f56e08995bf91a5abebdec34336b5e
aal2015 <abhinavlugun@yahoo.com>
2026-07-06 02:45:34 +0530

    Added unit tests for material insufficiency

--- ticTacToe_chess/src/tests/StaleMate.test.js
+++ ticTacToe_chess/src/tests/StaleMate.test.js
@@ -9,21 +9,16 @@ const emptyBoard = () =>
 
 describe("Stalemate detection", () => {
     it("detects basic stalemate", () => {
-
         const board = emptyBoard();
-
 
         // Black king a8
         board[0][0] = "bk";
-
 
         // White queen c7
         board[1][2] = "wq";
 
-
         // White king c6
         board[2][2] = "wk";
-
 
         const result =
             isStaleMate(
@@ -33,29 +28,20 @@ describe("Stalemate detection", () => {
                 0
             );
 
-
         expect(result).toBe(true);
-
     });
 
-
-
     it("returns false when king is in check", () => {
-
         const board = emptyBoard();
-
 
         // Black king g8
         board[0][6] = "bk";
 
-
         // White queen g7 attacks king
         board[1][6] = "wq";
 
-
         // White king g6
         board[2][6] = "wk";
-
 
         const result =
             isStaleMate(
@@ -64,30 +50,20 @@ describe("Stalemate detection", () => {
                 null,
                 0
             );
-
-
         expect(result).toBe(false);
-
     });
 
-
-
     it("returns false when a piece has a legal move", () => {
-
         const board = emptyBoard();
-
 
         // Black king a8
         board[0][0] = "bk";
 
-
         // Black rook h8
         board[0][7] = "br";
 
-
         // White king g6
         board[2][6] = "wk";
-
 
         const result =
             isStaleMate(
@@ -96,12 +72,76 @@ describe("Stalemate detection", () => {
                 null,
                 0
             );
-
-
         expect(result).toBe(false);
 
     });
 
+    it("detects draw by insufficient material (king vs king)", () => {
+        const board = emptyBoard();
 
+        board[0][0] = "bk";
+        board[7][7] = "wk";
 
+        expect(
+            isStaleMate(board, "white", null, 0)
+        ).toBe(true);
+
+        expect(
+            isStaleMate(board, "black", null, 0)
+        ).toBe(true);
+    });
+
+    it("detects draw by insufficient material (king and bishop vs king)", () => {
+        const board = emptyBoard();
+
+        board[0][0] = "bk";
+        board[7][7] = "wk";
+        board[6][6] = "wb";
+
+        expect(
+            isStaleMate(board, "black", null, 0)
+        ).toBe(true);
+    });
+
+    it("detects draw by insufficient material (king and knight vs king)", () => {
+        const board = emptyBoard();
+
+        board[0][0] = "bk";
+        board[7][7] = "wk";
+        board[6][6] = "wn";
+
+        expect(
+            isStaleMate(board, "black", null, 0)
+        ).toBe(true);
+    });
+
+    it("detects draw by insufficient material (bishop vs bishop)", () => {
+        const board = emptyBoard();
+
+        board[0][0] = "bk";
+        board[1][1] = "bb";
+
+        board[7][7] = "wk";
+        board[6][6] = "wb";
+
+        expect(
+            isStaleMate(board, "white", null, 0)
+        ).toBe(true);
+
+        expect(
+            isStaleMate(board, "black", null, 0)
+        ).toBe(true);
+    });
+
+    it("does not detect insufficient material when a pawn remains", () => {
+        const board = emptyBoard();
+
+        board[0][0] = "bk";
+        board[7][7] = "wk";
+        board[6][6] = "wp";
+
+        expect(
+            isStaleMate(board, "black", null, 0)
+        ).toBe(false);
+    });
 });
