9bdd0842cc69eade4b1c273b476f68afb44cdc28
aal2015 <abhinavlugun@yahoo.com>
2026-07-09 13:09:34 +0530

    Added terminal state check for min max algo. Added evaluate board for min max algo once depth limit reached

--- ticTacToe_chess/src/components/ChessAI.js
+++ ticTacToe_chess/src/components/ChessAI.js
@@ -1,31 +1,63 @@
 import {
-    isCheckMate
+    isCheckMate,
+    isStaleMate
 } from './moveValidCheck';
 
-
-const ChessPiecePoints = {
-    "queen": 9,
-    "rook": 5,
-    "bishop": 3,
-    "knight": 3,
-    "pawn": 3,
+const chessPiecePoints = {
+    p: 1,
+    n: 3,
+    b: 3,
+    r: 5,
+    q: 9,
+    k: 0
 };
 
 class ChessMinMaxAlgo {
-    constructor() {}
+    constructor() { }
 
-    minMax(board, pieceColor, enPassantState, castleState, moveCount) {
-        
+    evaluateBoard(board) {
+        let score = 0;
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                const piece = board[row][col];
+                if (piece === "") continue;
+
+                const value = chessPiecePoints[piece[1]];
+
+                if (piece[0] === "w") {
+                    score += value;
+                } else {
+                    score -= value;
+                }
+            }
+        }
+
+        return score;
     }
 
-    // isGameEnded(board, pieceColor, enPassantState, moveCount) {
-        
-    // }
+    minMax(board, pieceColor, enPassantState, castleState, moveCount, curDepth, MaxDepth) {
+        //// Terminal States
+        if (isCheckMate(
+            board, pieceColor,
+            enPassantState, moveCount + 1
+        )) {
+            if (pieceColor === "white") { // black wins
+                return -1000;
+            } else if (pieceColor === "black") { // white wins
+                return 1000;
+            }
+        };
 
-    // isCheckMate(
-    //                         boardClone,
-    //                         enemyColor,
-    //                         nextEnPassantState,
-    //                         moveCount
-    //                     );
+        if (isStaleMate(
+            board, pieceColor,
+            enPassantState, moveCount + 1
+        )) {
+            return 0;
+        }
+
+        if (curDepth == MaxDepth) {
+
+        }
+
+    }
 }
