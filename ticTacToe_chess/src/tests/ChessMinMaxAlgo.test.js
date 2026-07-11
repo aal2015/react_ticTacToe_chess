c70203ceb9f9a4f428ef57ca643d2e31a94aee2c
aal2015 <abhinavlugun@yahoo.com>
2026-07-11 14:56:15 +0530

    updated unit tests for terminal states for min max algo

--- ticTacToe_chess/src/tests/ChessMinMaxAlgo.test.js
+++ ticTacToe_chess/src/tests/ChessMinMaxAlgo.test.js
@@ -6,6 +6,15 @@ const algo = new ChessMinMaxAlgo();
 const emptyBoard = () =>
     Array.from({ length: 8 }, () => Array(8).fill(""));
 
+const castleState = {
+    whiteKingMoved: false,
+    blackKingMoved: false,
+    whiteLeftRookMoved: false,
+    whiteRightRookMoved: false,
+    blackLeftRookMoved: false,
+    blackRightRookMoved: false
+};
+
 describe("evaluateBoard", () => {
 
     it("returns 0 for king vs king", () => {
@@ -54,6 +63,7 @@ describe("evaluateBoard", () => {
 });
 
 describe("minMax terminal states", () => {
+
     it("returns board evaluation at max depth", () => {
         const board = emptyBoard();
 
@@ -61,20 +71,23 @@ describe("minMax terminal states", () => {
         board[7][7] = "wk";
         board[3][3] = "wq";
 
-        const score = algo.minMax(
+        const result = algo.minMax(
             board,
             "white",
             null,
-            {},
+            castleState,
             0,
             3,
             3
         );
 
-        expect(score).toBe(9);
+        expect(result).toEqual({
+            score: 9,
+            move: null
+        });
     });
 
-    it("returns -1000 when white is checkmated", () => {
+    it("returns negative score when white is checkmated", () => {
         const board = emptyBoard();
 
         // White king
@@ -86,20 +99,23 @@ describe("minMax terminal states", () => {
         // Black king protecting queen
         board[2][2] = "bk";
 
-        const score = algo.minMax(
+        const result = algo.minMax(
             board,
             "white",
             null,
-            {},
+            castleState,
             0,
             0,
             3
         );
 
-        expect(score).toBe(-1000);
+        expect(result).toEqual({
+            score: -1000,
+            move: null
+        });
     });
 
-    it("returns 1000 when black is checkmated", () => {
+    it("returns positive score when black is checkmated", () => {
         const board = emptyBoard();
 
         // Black king
@@ -111,20 +127,23 @@ describe("minMax terminal states", () => {
         // White king protecting queen
         board[2][2] = "wk";
 
-        const score = algo.minMax(
+        const result = algo.minMax(
             board,
             "black",
             null,
-            {},
+            castleState,
             0,
             0,
             3
         );
 
-        expect(score).toBe(1000);
+        expect(result).toEqual({
+            score: 1000,
+            move: null
+        });
     });
 
-    it("returns 0 for stalemate", () => {
+    it("returns draw score for stalemate", () => {
         const board = emptyBoard();
 
         // Black king a8
@@ -136,16 +155,48 @@ describe("minMax terminal states", () => {
         // White king c6
         board[2][2] = "wk";
 
-        const score = algo.minMax(
+        const result = algo.minMax(
             board,
             "black",
             null,
-            {},
+            castleState,
             0,
             0,
             3
         );
 
-        expect(score).toBe(0);
+        expect(result).toEqual({
+            score: 0,
+            move: null
+        });
     });
+
+    it("adjusts mate score based on depth", () => {
+        const board = emptyBoard();
+
+        // Black king
+        board[0][0] = "bk";
+
+        // White queen
+        board[1][1] = "wq";
+
+        // White king
+        board[2][2] = "wk";
+
+        const result = algo.minMax(
+            board,
+            "black",
+            null,
+            castleState,
+            0,
+            4,
+            6
+        );
+
+        expect(result).toEqual({
+            score: 996, // 1000 - 4
+            move: null
+        });
+    });
+
 });
